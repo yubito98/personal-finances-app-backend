d30cfcb367034003aaaf2ba912b911ff40832708
Yubor Molina <yubor98@gmail.com>
2025-01-28 17:16:17 -0500

    create method for transaction service is connected to the database

--- api/services/transactions.js
+++ api/services/transactions.js
@@ -7,13 +7,25 @@ class Transactions{
     }
 
     async read(){
-        const transactions = database;
-        const rta = await transactions.query('SELECT * FROM TRANSACTIONS')
+        const rta = await database.query('SELECT * FROM TRANSACTIONS')
         return rta.rows;
     }
 
-    create(body){
-        this.transactions.push(body)
+    async create(body){
+        try {
+            const query = `
+                INSERT INTO transactions (concept, value, type, category_id, date) 
+                VALUES ($1, $2, $3, $4, $5) 
+                RETURNING *;
+            `;
+            const values = [body.concept, body.value, body.type || "Expense", body.category_id || 1, body.date]; // Using placeholders
+    
+            const rta = await database.query(query, values);
+            return rta.rows[0]; // Return inserted row
+        } catch (error) {
+            console.error("Error inserting transaction:", error);
+            throw error; // Rethrow error for debugging
+        }
     }
 
     delete(id){
